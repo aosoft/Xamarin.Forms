558668b6396b7c4ba687519d49241ca07679cc40
aosoft <tan-y@aosoft.jp>
2017-07-19 13:43:06 +0900

    implement VisualElementRenderer

--- Xamarin.Forms.Platform.WinForms/ConvertExtensions.cs
+++ Xamarin.Forms.Platform.WinForms/ConvertExtensions.cs
@@ -1,23 +1,18 @@
-﻿using Windows.UI.Xaml.Media;
-
-#if WINDOWS_UWP
-
-namespace Xamarin.Forms.Platform.UWP
-#else
-
-namespace Xamarin.Forms.Platform.WinRT
-#endif
+﻿
+namespace Xamarin.Forms.Platform.WinForms
 {
 	internal static class ConvertExtensions
 	{
+		/*
 		public static Brush ToBrush(this Color color)
 		{
 			return new SolidColorBrush(color.ToWindowsColor());
 		}
+		*/
 
-		public static Windows.UI.Color ToWindowsColor(this Color color)
+		public static System.Drawing.Color ToWindowsColor(this Color color)
 		{
-			return Windows.UI.Color.FromArgb((byte)(color.A * 255), (byte)(color.R * 255), (byte)(color.G * 255), (byte)(color.B * 255));
+			return System.Drawing.Color.FromArgb((byte)(color.A * 255), (byte)(color.R * 255), (byte)(color.G * 255), (byte)(color.B * 255));
 		}
 	}
 }
--- Xamarin.Forms.Platform.WinForms/VisualElementRenderer.cs
+++ Xamarin.Forms.Platform.WinForms/VisualElementRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Xamarin.Forms.Platform.WinForms
 	public class VisualElementRenderer<TElement, TNativeElement> :
 		Form, IVisualElementRenderer, IDisposable, IEffectControlProvider
 		where TElement : VisualElement
+		where TNativeElement : Control
 	{
 
 		protected override void Dispose(bool disposing)
@@ -17,17 +19,103 @@ namespace Xamarin.Forms.Platform.WinForms
 			base.Dispose(disposing);
 		}
 
+		public TNativeElement Control { get; private set; }
+
+		public TElement Element { get; private set; }
+
+
+
+		protected virtual void OnElementChanged(ElementChangedEventArgs<TElement> e)
+		{
+			var args = new VisualElementChangedEventArgs(e.OldElement, e.NewElement);
+			ElementChanged?.Invoke(this, args);
+		}
+
+		protected virtual void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == VisualElement.IsEnabledProperty.PropertyName)
+				UpdateEnabled();
+			else if (e.PropertyName == VisualElement.BackgroundColorProperty.PropertyName)
+				UpdateBackgroundColor();
+			/*
+			else if (e.PropertyName == AutomationProperties.HelpTextProperty.PropertyName)
+				SetAutomationPropertiesHelpText();
+			else if (e.PropertyName == AutomationProperties.NameProperty.PropertyName)
+				SetAutomationPropertiesName();
+			else if (e.PropertyName == AutomationProperties.IsInAccessibleTreeProperty.PropertyName)
+				SetAutomationPropertiesAccessibilityView();
+			else if (e.PropertyName == AutomationProperties.LabeledByProperty.PropertyName)
+				SetAutomationPropertiesLabeledBy();
+			*/
+		}
+
+		protected virtual void UpdateBackgroundColor()
+		{
+			Color backgroundColor = Element.BackgroundColor;
+			var control = Control as Control;
+			if (control != null)
+			{
+				if (!backgroundColor.IsDefault)
+				{
+					control.BackColor = backgroundColor.ToWindowsColor();
+				}
+				else
+				{
+					control.BackColor = System.Drawing.SystemColors.Window;
+				}
+			}
+			else
+			{
+				if (!backgroundColor.IsDefault)
+				{
+					BackColor = backgroundColor.ToWindowsColor();
+				}
+				else
+				{
+					BackColor = System.Drawing.SystemColors.Window;
+				}
+			}
+		}
+
+		protected virtual void UpdateNativeControl()
+		{
+			UpdateEnabled();
+			/*
+			SetAutomationPropertiesHelpText();
+			SetAutomationPropertiesName();
+			SetAutomationPropertiesAccessibilityView();
+			SetAutomationPropertiesLabeledBy();
+			*/
+		}
+
+		void UpdateEnabled()
+		{
+			var control = Control as Control;
+			if (control != null)
+				control.Enabled = Element.IsEnabled;
+			/*else
+				IsHitTestVisible = Element.IsEnabled && !Element.InputTransparent;*/
+		}
+
+		void UpdateTracker()
+		{
+			/*
+			if (_tracker == null)
+				return;
+
+			_tracker.PreventGestureBubbling = PreventGestureBubbling;
+			_tracker.Control = Control;
+			_tracker.Element = Element;
+			_tracker.Container = ContainerElement;
+			*/
+		}
+
+
 		#region IVisualElementRenderer
 
 		public Control ContainerElement => this;
 
-		public VisualElement Element
-		{
-			get
-			{
-				throw new NotImplementedException();
-			}
-		}
+		VisualElement IVisualElementRenderer.Element => Element;
 
 		public event EventHandler<VisualElementChangedEventArgs> ElementChanged;
 
@@ -38,19 +126,62 @@ namespace Xamarin.Forms.Platform.WinForms
 
 		public Control GetNativeElement()
 		{
-			throw new NotImplementedException();
+			return Control;
 		}
 
 		public void SetElement(VisualElement element)
 		{
-			throw new NotImplementedException();
+			/*
+			TElement oldElement = Element;
+			Element = (TElement)element;
+
+			if (oldElement != null)
+			{
+				oldElement.PropertyChanged -= OnElementPropertyChanged;
+				oldElement.FocusChangeRequested -= OnElementFocusChangeRequested;
+			}
+
+			if (element != null)
+			{
+				Element.PropertyChanged += OnElementPropertyChanged;
+				Element.FocusChangeRequested += OnElementFocusChangeRequested;
+
+				if (AutoPackage && Packager == null)
+					Packager = new VisualElementPackager(this);
+
+				if (AutoTrack && Tracker == null)
+				{
+					Tracker = new VisualElementTracker<TElement, TNativeElement>();
+				}
+
+				// Disabled until reason for crashes with unhandled exceptions is discovered
+				// Without this some layouts may end up with improper sizes, however their children
+				// will position correctly
+				//Loaded += (sender, args) => {
+				if (Packager != null)
+					Packager.Load();
+				//};
+			}
+
+			OnElementChanged(new ElementChangedEventArgs<TElement>(oldElement, Element));
+
+			var controller = (IElementController)oldElement;
+			if (controller != null && controller.EffectControlProvider == this)
+			{
+				controller.EffectControlProvider = null;
+			}
+
+			controller = element;
+			if (controller != null)
+				controller.EffectControlProvider = this;
+			*/
 		}
 
 		#endregion
 
 		#region IEffectControlProvider
 
-		public void RegisterEffect(Effect effect)
+		void IEffectControlProvider.RegisterEffect(Effect effect)
 		{
 			throw new NotImplementedException();
 		}
