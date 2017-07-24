c9f8385ed275d558986a7e1dc5df121009bc9d7b
aosoft <tan-y@aosoft.jp>
2017-07-24 13:48:45 +0900

    add Platform

---
+++ Xamarin.Forms.Platform.WinForms/Platform.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Platform.WinForms
+{
+	public class Platform : IPlatform, INavigation, IDisposable
+	{
+
+		#region IDisposable Support
+		private bool disposedValue = false; // 重複する呼び出しを検出するには
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: マネージ状態を破棄します (マネージ オブジェクト)。
+				}
+
+				// TODO: アンマネージ リソース (アンマネージ オブジェクト) を解放し、下のファイナライザーをオーバーライドします。
+				// TODO: 大きなフィールドを null に設定します。
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: 上の Dispose(bool disposing) にアンマネージ リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします。
+		// ~Platform() {
+		//   // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+		//   Dispose(false);
+		// }
+
+		// このコードは、破棄可能なパターンを正しく実装できるように追加されました。
+		public void Dispose()
+		{
+			// このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+			Dispose(true);
+			// TODO: 上のファイナライザーがオーバーライドされる場合は、次の行のコメントを解除してください。
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
+
+
+		public IReadOnlyList<Page> ModalStack
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public IReadOnlyList<Page> NavigationStack
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public SizeRequest GetNativeSize(VisualElement view, double widthConstraint, double heightConstraint)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void InsertPageBefore(Page page, Page before)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Page> PopAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Page> PopAsync(bool animated)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Page> PopModalAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Page> PopModalAsync(bool animated)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task PopToRootAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task PopToRootAsync(bool animated)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task PushAsync(Page page)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task PushAsync(Page page, bool animated)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task PushModalAsync(Page page)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task PushModalAsync(Page page, bool animated)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemovePage(Page page)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
