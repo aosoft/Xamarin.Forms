5501ac4f8ac0ecaa27c2e762046938bf42e7161a
aosoft <tan-y@aosoft.jp>
2017-09-17 22:21:08 +0900

    removed files

--- Xamarin.Forms.Platform.Android/AppCompat/FrameRenderer.cs
+++
@@ -1,8 +0,0 @@
-namespace Xamarin.Forms.Platform.Android.AppCompat
-{
-    // This version of FrameRenderer is here for backward compatibility with anyone referencing 
-    // FrameRenderer from this namespace
-    public class FrameRenderer : FastRenderers.FrameRenderer
-    {
-    }
-}
