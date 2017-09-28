cba0adac597804383494fa68f49c32f4fe0b6326
aosoft <tan-y@aosoft.jp>
2017-09-29 06:25:23 +0900

    removed files

--- Xamarin.Forms.Platform.Android/AppCompat/ViewRenderer.cs
+++
@@ -1,6 +0,0 @@
-namespace Xamarin.Forms.Platform.Android.AppCompat
-{
-	public abstract class ViewRenderer<TView, TControl> : Android.ViewRenderer<TView, TControl> where TView : View where TControl : global::Android.Views.View
-	{
-	}
-}
