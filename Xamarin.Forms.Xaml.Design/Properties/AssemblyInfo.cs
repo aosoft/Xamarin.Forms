f195d09a21f144ecf7f2a16576805d6e418cf314
aosoft <tan-y@aosoft.jp>
2017-11-13 23:40:44 +0900

    removed files

--- Xamarin.Forms.Xaml.Design/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using Microsoft.Windows.Design.Metadata;
-using Xamarin.Forms.Xaml.Design;
-
-[assembly: AssemblyTitle("Xamarin.Forms.Xaml.Design")]
-[assembly: AssemblyDescription("Provides the design-time metadata for the XAML language service.")]
-[assembly: ProvideMetadata(typeof (RegisterMetadata))]
