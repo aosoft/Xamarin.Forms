c74b4b49dd1ed63e09991eca5ae7cff9356602cf
aosoft <tan-y@aosoft.jp>
2017-12-21 21:39:59 +0900

    added GTK Sample

---
+++ XF-GtkWpf/GtkApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using GLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Platform.GTK;
+
+namespace GtkApp
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			ExceptionManager.UnhandledException += OnUnhandledException;
+
+			GtkOpenGL.Init();
+			GtkThemes.Init();
+			Gtk.Application.Init();
+			Xamarin.Forms.Forms.Init();
+			var app = new SharedApp.App();
+			var window = new FormsWindow();
+			window.LoadApplication(app);
+			window.SetApplicationTitle("Xamarin.Forms GTK# Backend");
+			window.Show();
+			Gtk.Application.Run();
+		}
+		private static void OnUnhandledException(UnhandledExceptionArgs args)
+		{
+			System.Diagnostics.Debug.WriteLine($"Unhandled GTK# exception: {args.ExceptionObject}");
+		}
+	}
+}
