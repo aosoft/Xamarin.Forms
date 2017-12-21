1314465914f7f44e77ce12813ee1d7e183a52998
aosoft <tan-y@aosoft.jp>
2017-12-21 21:24:51 +0900

    added WPF Sample

---
+++ XF-GtkWpf/SharedApp/App.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedApp
+{
+	public class App : Xamarin.Forms.Application
+	{
+		public App()
+		{
+			MainPage = new Page1();
+		}
+
+		protected override void OnStart()
+		{
+			base.OnStart();
+		}
+
+		protected override void OnSleep()
+		{
+			base.OnSleep();
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+		}
+	}
+}
